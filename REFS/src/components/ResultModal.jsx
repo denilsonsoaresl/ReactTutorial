14e7713a753bfb35f4ab8fe17530c376b3728b17
denilsonsoaresl <denilson.soares@lumnieducacao.com.br>
2024-10-22 20:30:20 -0300

    finalizado cap8

--- REFS/src/components/ResultModal.jsx
+++ REFS/src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import {createPortal} from 'react-dom';
 
 const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, onReset }, ref) {
 
@@ -14,7 +15,7 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
         }
     }));
 
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -23,7 +24,8 @@ const ResultModal = forwardRef(function ResultModal({targetTime, remainingTime, 
             <form method="dialog">
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     );
 });
 
