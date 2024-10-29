f448a9372981dcaafaade93a9247114756e3f8f5
denilsonsoaresl <denilson.soares@lumnieducacao.com.br>
2024-10-28 21:00:42 -0300

    Making Projects Selectable & Viewing Project Details

--- CAP9/src/components/NoProjectSelected.jsx
+++ CAP9/src/components/NoProjectSelected.jsx
@@ -8,7 +8,7 @@ export default function NoProjectSelected({onStartAddProject}){
                 src={noProjectImage} 
                 alt="An empty task list" 
                 className="w-16 h-16 object-contain mx-auto"/>
-            <h2 className="text-xl font-bold text-stone-500 mt-4 ">No Project Selected</h2>
+            <h2 className="text-xl font-bold text-stone-500 mt-4">No Project Selected</h2>
             <p className="text-stone-400 mb-4">
                 Select a project or get started with a new one
             </p>
