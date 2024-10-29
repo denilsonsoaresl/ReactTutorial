d5ce59bf83ba885ab636fed0ce76bc8ab0a94c42
denilsonsoaresl <denilson.soares@lumnieducacao.com.br>
2024-10-29 20:29:53 -0300

    finalizado

--- CAP9/src/App.jsx
+++ CAP9/src/App.jsx
@@ -8,8 +8,29 @@ import SelectedProject from "./components/SelectedProject";
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
+    tasks: []
   });
+
+  function handleAddTask(text){
+    setProjectsState(prevState=>{
+      const taskId = Math.random();
+      const newTask = {
+        text: text,
+        projectId:prevState.selectedProjectId,
+        id: taskId,
+      };
+
+      return{
+        ...prevState,
+        tasks:[newTask, ...prevState.tasks]
+      };
+    });
+  }
+
+  function handleDeleteTask(){
+
+  }
 
   function handleSelectProject(id){
     setProjectsState(prevState => {
@@ -70,7 +91,16 @@ function App() {
     project => project.id === projectsState.selectedProjectId
   );
 
-  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>;
+  let content = (
+    <SelectedProject 
+      project={selectedProject} 
+      onDelete={handleDeleteProject} 
+      onAddTask={handleAddTask}
+      onDeleteTask={handleDeleteTask}
+      tasks={projectsState.tasks}
+    />
+      
+  );
 
   if (projectsState.selectedProjectId === null ){
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject}/>
